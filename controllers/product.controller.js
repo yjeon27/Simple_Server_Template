8c4286ef9e5d6c5fa23c8d77ceddc7743a866307
Young Jeon <yjeon27@uwo.ca>
2019-10-16 16:51:53 -0400

    Read

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -4,3 +4,24 @@ const Product = require('../models/product.model');
 exports.test = function (req, res) {
     res.send('Greetings from the Test controller!');
 };
+
+exports.create = function (req, res) {
+    let product = new Product({
+        name: req.body.name,
+        price: req.body.price
+    });
+
+    product.save(function(err) {
+        if(err){
+            return next(err);
+        }
+        res.send("Product Created Successfully");
+    });
+};
+
+exports.details = function (req, res){
+    Product.findById(req.params.id, function (err, product){
+        if(err) return next(err);
+        res.send(product);
+    })
+}
