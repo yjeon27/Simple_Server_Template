152632d5cf1e991cd67dc2ef7e8395423eb6eeef
Young Jeon <yjeon27@uwo.ca>
2019-10-26 02:51:10 -0400

    get item by specific ID + poll entire DB every 5 seconds

--- client/script.js
+++ client/script.js
@@ -3,6 +3,7 @@ window.onload=function(){
     const addForm = document.getElementById("addItemForm"),
         updateForm = document.getElementById("updateForm"),
         deleteForm = document.getElementById("deleteForm"),
+        getForm = document.getElementById("getForm"),
         addTitle = document.getElementById("titleInput"),
         addType = document.getElementById("type"),
         addDue = document.getElementById("due"),
@@ -10,6 +11,7 @@ window.onload=function(){
         getDB = document.getElementById("getDB"),
         updateID = this.document.getElementById("updateID"),
         deleteID = this.document.getElementById("deleteID"),
+        getID = this.document.getElementById("getID"),
         updateTitle = this.document.getElementById("updateTitle"),
         updateType = this.document.getElementById("updateType"),
         updateDue = this.document.getElementById("updateDue"),
@@ -18,30 +20,57 @@ window.onload=function(){
 
 
     getDB.addEventListener("click", function(){
-        // debugger;
+        function intervalUpdate(){
+            const http = new XMLHttpRequest();
+            http.open("GET", url+"getAll", true);
+            http.onload = function(){
+                let obj = JSON.parse(http.responseText);
+                const itemsList = document.getElementById("itemsList");
+    
+                while(itemsList.firstChild){
+                    itemsList.removeChild(itemsList.firstChild);
+                }
+    
+                for(let i=0; i<obj.length; i++){
+                    const li = document.createElement("li");
+                    li.setAttribute("id", "li"+i);
+                    const tempNode = document.createTextNode(JSON.stringify(obj[i]));
+                    li.appendChild(tempNode);
+                    itemsList.appendChild(li);
+                }
+            }
+            http.send();
+
+            setTimeout(intervalUpdate, 5000);
+        }
+        // setInterval(intervalUpdate, 5000);
+        intervalUpdate();
+    });
+
+    getForm.addEventListener("submit", function(e){
         const http = new XMLHttpRequest();
-        http.open("GET", url+"getAll", true);
+        e.preventDefault();
+
+        http.open("GET", url+getID.value, true);
         http.onload = function(){
+            // debugger
             let obj = JSON.parse(http.responseText);
             const itemsList = document.getElementById("itemsList");
 
             while(itemsList.firstChild){
                 itemsList.removeChild(itemsList.firstChild);
             }
-
-            for(let i=0; i<obj.length; i++){
-                const li = document.createElement("li");
-                li.setAttribute("id", "li"+i);
-                const tempNode = document.createTextNode(JSON.stringify(obj[i]));
-                li.appendChild(tempNode);
-                itemsList.appendChild(li);
-            }
-        }
+            const li = document.createElement("li");
+            li.setAttribute("id", "li0");
+            const tempNode = document.createTextNode(JSON.stringify(obj));
+            li.appendChild(tempNode);
+            itemsList.appendChild(li);
+        };
         http.send();
     });
 
     deleteForm.addEventListener("submit", function(e){
-        debugger
+        // debugger
         const http = new XMLHttpRequest();
         e.preventDefault();
 
@@ -50,7 +79,7 @@ window.onload=function(){
             console.log('DELETING: '+url+deleteID.value+"/delete");
         };
         http.send(deleteID.value);
-    })
+    });
 
     updateForm.addEventListener("submit", function(e){
         const http = new XMLHttpRequest();
