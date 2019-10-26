539d15a62a4ac61ce49882ebdb326520f981c163
EC2 Default User <ec2-user@ip-172-31-38-38.ec2.internal>
2019-10-26 06:53:40 +0000

    more headers for CORS validation

--- server.js
+++ server.js
@@ -22,8 +22,15 @@ db.on('error', err => {
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(function(req, res, next){
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+})
 app.use('/items', item);
 
+
+console.log('process.env.PORT: '+process.env.PORT);
 
 // ACCESSED BY "http://34.201.141.150:8080/items/test"
 app.listen(process.env.PORT);
